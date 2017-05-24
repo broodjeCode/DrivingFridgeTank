7af8720b98749fd4b44ff2cc213f73bfd9ae0e77
rastabilly <milchenka@gmail.com>
2017-05-24 20:04:22 +0000

    Added commandline arguments to test.py\nAdded Camera.py basics from earlier example developed outside git tree

--- Camera.py
+++ Camera.py
@@ -1,9 +1,147 @@
-class Camera
+import sys
+import os
+from multiprocessing import Process, Queue, Manager
+from time import sleep as sleep
+import time
+import numpy as np
+import cv2
+import picamera
+import picamera.array
+import imutils
+from collections import deque
 
 
+class cameraThread(Process):
+        def __init__(self):
+                self.debug=False
+
+	def cameraThread(self, args):
+		try:
+			with picamera.PiCamera() as camera:
+				with picamera.array.PiRGBArray(camera) as stream:
+					## Setup camera capture resolution
+					camera.resolution = (args["cwidth"],args["cheight"])
+	
+					## setup vars from args
+					cameraResolution=(args["cwidth"],args["cheight"])
+					processingResolution=(args["pwidth"],args["pheight"])
+					daemon=args["daemon"]
+					debug=args["cdebug"]
+					pts = deque(maxlen=args["buffer"])
+
+					## create window colorbars
+					cv2.namedWindow('Colorbars')
+					hh='Hue High'
+					hl='Hue Low'
+					sh='Saturation High'
+					sl='Saturation Low'
+					vh='Value High'
+					vl='Value Low'
+					wnd = 'Colorbars'
+					cv2.createTrackbar(hl, wnd,155,179,nothing)
+					cv2.createTrackbar(hh, wnd,179,179,nothing)
+					cv2.createTrackbar(sl, wnd,123,255,nothing)
+					cv2.createTrackbar(sh, wnd,255,255,nothing)
+					cv2.createTrackbar(vl, wnd,72,255,nothing)
+					cv2.createTrackbar(vh, wnd,190,255,nothing)
+
+					while True: ## LOOP
+						## reset empty vars
+						radius=0
+
+						## Set values from colorbars
+						hul=cv2.getTrackbarPos(hl, wnd)
+						huh=cv2.getTrackbarPos(hh, wnd)
+						sal=cv2.getTrackbarPos(sl, wnd)
+						sah=cv2.getTrackbarPos(sh, wnd)
+						val=cv2.getTrackbarPos(vl, wnd)
+						vah=cv2.getTrackbarPos(vh, wnd)
+						HSVLOW=np.array([hul,sal,val])
+						HSVHIGH=np.array([huh,sah,vah])
+
+						## capture from camera
+						camera.capture(stream, 'bgr', use_video_port=True)
+
+						# stream.array now contains the image data in BGR order
+						frame = stream.array
+						frame = imutils.resize(frame, width=args["pwidth"], height=args["pheight"])
+
+						blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+						if debug:
+							cv2.imshow('blur', blurred)
+							if cv2.waitKey(1) & 0xFF == ord('q'):
+								break
+
+						hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+						if debug:
+							cv2.imshow('hsv', hsv)
+							if cv2.waitKey(1) & 0xFF == ord('q'):
+								break
+
+						mask = cv2.inRange(hsv, HSVLOW, HSVHIGH)
+						mask = cv2.erode(mask, None, iterations=2)
+						mask = cv2.dilate(mask, None, iterations=2)
+
+						if debug:
+							cv2.imshow('mask', mask)
+							if cv2.waitKey(1) & 0xFF == ord('q'):
+								break
+
+						cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+						cv2.CHAIN_APPROX_SIMPLE)[-2]
+						center = None
+
+						## If cnts > 0 Target locked!
+						if len(cnts) > 0:
+							objectDetected=True
+							# find the largest contour in the mask, then use
+							# it to compute the minimum enclosing circle and
+							# centroid
+							c = max(cnts, key=cv2.contourArea)
+							((x, y), radius) = cv2.minEnclosingCircle(c)
+							M = cv2.moments(c)
+							center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+							# only proceed if the radius meets a minimum size
+							if radius > 2 and debug == True:
+								# draw the circle and centroid on the frame,
+								# then update the list of tracked points
+								cv2.circle(frame, (int(x), int(y)), int(radius),
+									(0, 255, 255), 2)
+								cv2.circle(frame, center, 5, (0, 0, 255), -1)
+						else:
+							objectDetected=False
+						pts.appendleft(center)
+
+						## Print location to console relative to image resolution from topleft to bottom right
+						objectLocation=center
+
+						if debug:
+							for i in xrange(1, len(pts)):
+							# if either of the tracked points are None, ignore
+							# them
+								if pts[i - 1] is None or pts[i] is None:
+									continue
+
+								# otherwise, compute the thickness of the line and
+								# draw the connecting lines
+								thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+								cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
 
+						## Display output
+						if daemon == False:
+							cv2.imshow('frame', frame)
+							if cv2.waitKey(1) & 0xFF == ord('q'):
+								break
 
+						## publish data to queue
+#						ocvQueue.put([cameraResolution, processingResolution, objectLocation, objectDetected, radius])
 
-
-	def __init__(self):
+						# reset the stream before the next capture
+						stream.seek(0)
+						stream.truncate()
+					raise Exception('Quit')
+		except :
+			print "cameraThread() CTRL+C"
--- test.py
+++ test.py
@@ -2,8 +2,39 @@ from multiprocessing import Process, Queue, Manager, Value, Array
 from time import sleep as sleep
 import Ultrasone
 import Camera
+import argparse
+
+## set program version
+pName="test"
+pVersion="0.1"
 
 def main():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-d", "--daemon", help="daemonize program", type=bool, default=False)
+	ap.add_argument("-D", "--debug", help="general program debugging", type=bool, default=False)
+	ap.add_argument("-DS", "--sdebug", help="sensor debugging", type=bool, default=False)
+	ap.add_argument("-DC", "--cdebug", help="Camera debugging", type=bool, default=False)
+	ap.add_argument("-cw", "--cwidth", help="Camera Resolution width", type=int, default=320)
+	ap.add_argument("-ch", "--cheight", help="Camera Resolution height", type=int, default=240)
+	ap.add_argument("-pw", "--pwidth", help="Processing Resolution width", type=int, default=320)
+	ap.add_argument("-ph", "--pheight", help="Processing Resolution height", type=int, default=240)
+	ap.add_argument("-b", "--buffer", help="tracing buffer", type=int, default=64)
+	args=vars(ap.parse_args())
+
+	print "Starting %s %s" % (str(pName), str(pVersion))
+	print "-=====[ Options ]=====-"
+	print "--daemon:    %s" % str(args["daemon"])
+	print "--debug:     %s" % str(args["debug"])
+	print "--sdebug:    %s" % str(args["sdebug"])
+	print "--cdebug:    %s" % str(args["cdebug"])
+	print "--cwidth:    %s" % str(args["cwidth"])
+	print "--cheight:   %s" % str(args["cheight"])
+	print "--pwidth:    %s" % str(args["pwidth"])
+	print "--pheight:   %s" % str(args["pheight"])
+	print "--buffer:    %s" % str(args["buffer"])
+
+	sleep(2)
+
 	manager = Manager()
 
 
@@ -20,8 +51,9 @@ def main():
 
 
 	ultra=Ultrasone.Ultrasone()
-	camera=Camera.
+	camera=Camera.cameraThread()
 	sensorThread=Process(target=ultra.run, args=(distanceValue,))
+	cameraThread=Process(target=camera.run, args=(args,))
 	sensorThread.start()
 
 
