071f706fc726d22a757dca297693a6e151e3161e
rastabilly <milchenka@gmail.com>
2017-05-26 15:41:41 +0000

    First try to get/set from classes to one another

--- PerimeterIntel.py
+++ PerimeterIntel.py
@@ -1,38 +1,53 @@
+from multiprocessing import Process
+import time
+
 import Ultrasone
 
-class PerimeterIntel
+class PerimeterIntel(Process):
 
-	isWallFront = False
-	isWallLeft = False
-	isWallRight = False
+	def __init__(self,ultra,image):
+		debug=False
+		self.ultra=ultra
+		self.image=image
 
-
-
-
-	UltrasoneLeft = Ultrasone()
-	UltrasoneRight = Ultrasone()
-	ip = ImageProcessor()
-
-	def __init__(self):
-
-
-	
+	def run(self):
+		while True:
+			time.sleep(1)
+			print str(self.ultra.getDist())
 
 
 
-	def getObjectInSIght
-		return ImageProcessor.objectInSight
-
-	def getDistLeft()
-		return 
-
-	def getDistRight()
-		pass
-
-
-
-
-
-	while(true)
-		UltrasoneLeft.getDist()
-		UltrasoneRight.getDist()
+#	isWallFront = False
+#	isWallLeft = False
+#	isWallRight = False
+#
+#
+#
+#
+#	UltrasoneLeft = Ultrasone()
+#	UltrasoneRight = Ultrasone()
+#	ip = ImageProcessor()
+#
+#	def __init__(self):
+#
+#
+#	
+#
+#
+#
+#	def getObjectInSIght
+#		return ImageProcessor.objectInSight
+#
+#	def getDistLeft()
+#		return 
+#
+#	def getDistRight()
+#		pass
+#
+#
+#
+#
+#
+#	while(true)
+#		UltrasoneLeft.getDist()
+#		UltrasoneRight.getDist()
