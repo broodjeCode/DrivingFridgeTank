broodjeCode/DrivingFridgeTank

commit b7e87b632d66ced9e9079cf34a78d419d351bef5

Few fixes, comments and code police

Robot.py
@@ -1,28 +1,30 @@
-from multiprocessing import Process, Manager, Value, Array
-import time
-import argparse
-
-import Ultrasone
-import ImageProcessor
-import PerimeterIntel
+from multiprocessing import Process, Manager, Value, Array	# Used for handling multicore processing
+import time							# Used for timing and sleep functions
+import argparse							# Used for parsing commandline arguments
+import Ultrasone						# Ultrasone functions	(Detect distance to objects)
+import ImageProcessor						# OpenCV functions	(process camera and detect object)
+import PerimeterIntel						# Perimeter functions	(basic driving intelligence like obstacle detection based on Ultrasone)
 
 ## set program version
-pName="test"
-pVersion="0.1"
+pName="DrivingFridgeRobot"
+pVersion="0.2"
 
-def main():  #link naar argument parser page, uit ball tracking code
+def main():  
+## Use argparse to pass options/variables to main process and child processes (Used parts of the code on http://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/ as reference for my argument parser)
+	# Add arguments
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-d", "--daemon", help="daemonize program", type=bool, default=False)
-	ap.add_argument("-D", "--debug", help="general program debugging", type=bool, default=False)
-	ap.add_argument("-DS", "--sdebug", help="sensor debugging", type=bool, default=False)
-	ap.add_argument("-DC", "--cdebug", help="Camera debugging", type=bool, default=False)
+	ap.add_argument("-d", "--daemon", help="daemonize program", type=bool, default=False) ## no gui is loaded
+	ap.add_argument("-D", "--debug", help="Enable general program debugging", type=bool, default=False)
+	ap.add_argument("-DS", "--sdebug", help="Enable sensor debugging", type=bool, default=False)
+	ap.add_argument("-DC", "--cdebug", help="Enable camera debugging", type=bool, default=False)
 	ap.add_argument("-cw", "--cwidth", help="Camera Resolution width", type=int, default=320)
 	ap.add_argument("-ch", "--cheight", help="Camera Resolution height", type=int, default=240)
 	ap.add_argument("-pw", "--pwidth", help="Processing Resolution width", type=int, default=320)
 	ap.add_argument("-ph", "--pheight", help="Processing Resolution height", type=int, default=240)
-	ap.add_argument("-b", "--buffer", help="tracing buffer", type=int, default=64)
+	ap.add_argument("-b", "--buffer", help="tracing buffer (red line in gui tracing object)", type=int, default=64)
 	args=vars(ap.parse_args())
 
+	# Print settings on startup
 	print "Starting %s %s" % (str(pName), str(pVersion))
 	print "-=====[ Options ]=====-"
 	print "--daemon:    %s" % str(args["daemon"])
@@ -35,63 +37,70 @@ def main():  #link naar argument parser page, uit ball tracking code
 	print "--pheight:   %s" % str(args["pheight"])
 	print "--buffer:    %s" % str(args["buffer"])
 
+	# Give us some time to actually read the arguments before the program starts
 	time.sleep(2)
 
-        ## Init Data arrays for trasferring data over smp threads (somewhat simple IPC)
-        ## DataManager
+## Init Data arrays for trasferring data over smp threads (somewhat simple IPC)
+        # DataManager
+        dataManager=Manager() ## Use Manager from multiprocessing to synchronize data objects (basically pass proxies to threads instead of actual arrays, the server is in the parent program)
 
-        dataManager=Manager() ## Use dataManager so synchronize data objects (basically pass proxies to threads instead of actual arrays, the server is in the parent program)
-
-	UltrasoneDataOut=dataManager.Array('f', range(4))
+        # Array objects, used for transporting data using proxies to communicate between threads (IPC part)
+	UltrasoneDataOut=dataManager.Array('f', range(7))
 	ImageProcessorDataOut=dataManager.Array('f', range(9))
-	PerIntelDataIn=dataManager.Array('f', range(3))
+	PerIntelDataIn=dataManager.Array('f', range(4))
 	PerIntelDataOut=dataManager.Array('f', range(3))
 
-	## Initialize and start processes
-
+## Initialize and start processes
 	# UltrasoneThread
-	ultra=Ultrasone.Ultrasone(args)
-	ultrasoneThread=Process(target=ultra.run, args=(UltrasoneDataOut,))
-	ultrasoneThread.start()
+	ultra=Ultrasone.Ultrasone(args)	# Create ultra object from Ultrasone class with args
+	ultrasoneThread=Process(target=ultra.run, args=(UltrasoneDataOut,)) # Create ultrasoneThread to run function "run" of Ultrasone.Ultrasone. Parse UltrasoneDataOut proxy to store values
+	ultrasoneThread.start() # Now start the thread!
 
 	# CameraThread
-	imageProcessor=ImageProcessor.ImageProcessor(args)
-	imageProcessorThread=Process(target=imageProcessor.run, args=(ImageProcessorDataOut,))
-	imageProcessorThread.start()
+	imageProcessor=ImageProcessor.ImageProcessor(args) # Create imageProcessor object from imageProcessor class with args
+	imageProcessorThread=Process(target=imageProcessor.run, args=(ImageProcessorDataOut,)) # Create imageProcessorThread to run function "run" of imageProcessor.ImageProcessor. Parse ImageProcessorDataOut proxy to store values
+	imageProcessorThread.start() # Now start this thread also!
 
 	# PerimeterIntelThread
-	perIntel=PerimeterIntel.PerimeterIntel(args)
-	perIntelThread=Process(target=perIntel.run, args=(PerIntelDataIn, PerIntelDataOut,)) ## Initialize PerIntelThread and parse PerIntelDataIn and PerIntelDataOut
-	perIntelThread.start()
+	perIntel=PerimeterIntel.PerimeterIntel(args) # Create perIntel object from PerimeterIntel class with args
+	perIntelThread=Process(target=perIntel.run, args=(PerIntelDataIn, PerIntelDataOut,)) ## Create PerIntelThread thread to run function "run" of PerimeterIntel.PerimeterIntel. Pass PerIntelDataIn and PerIntelDataOut proxies to process and store values
+	perIntelThread.start() # Fire!
 
-
-
-
+## Timing settings for displaying statistics on console and transferring data to proxies
+	# Last time the display was updated
 	lastDisplayUpdate=0
+        # Last time in us that the proxies were updated.
 	lastDataUpdate=0
+        # Print stats every 500ms
 	displayUpdateSpeed=0.500
-	dataUpdateSpeed=0.1
+        # Update data proxies every 10ms
+	dataUpdateSpeed=0.01
+
+## Here starts the main loop
 	try:
-		while True:	## Main loop for basic stuff and data transmissions between threads
-			time.sleep(0.001) ## don't hog the cpu
-			#DATABROKER - if lastDateUpdate is lower than current epoch time, rephresh
+		while True: ## Main loop for displaying statistics and data handling transmissions between threads
+			time.sleep(0.001) ## don't hog the cpu (atleast set some sort of limit to prevent the CPU from flooding with the while True loop
+
+			# dataManager - if lastDateUpdate is lower than current epoch time, then update data values. This must happen often but not continuesly.
 			if lastDataUpdate+dataUpdateSpeed < time.time():
-				#set current epoch
+				# Get current epoch time in us
 				lastDataUpdate=time.time()
-				#print "---DATAUPDATE---"
-				## Data handling/ipc shizzle
-				# PerIntelDataIn:
-				PerIntelDataIn[0]=UltrasoneDataOut[0]
-				PerIntelDataIn[1]=UltrasoneDataOut[1]
-				PerIntelDataIn[2]=UltrasoneDataOut[2]
+
+				# Pass data from various sources to PerIntelDataIn:
+				PerIntelDataIn[0]=UltrasoneDataOut[0]      # Left Ultrasone sensor values
+				PerIntelDataIn[1]=UltrasoneDataOut[1]      # Front Ultrasone sensor values
+				PerIntelDataIn[2]=UltrasoneDataOut[2]      # Right Ultrasone sensor values
+				PerIntelDataIn[3]=ImageProcessorDataOut[4] # Object detected
+				PerIntelDataIn[4]=ImageProcessorDataOut[5] # Object radius
+				PerIntelDataIn[5]=ImageProcessorDataOut[6] # Object location X
+				PerIntelDataIn[6]=ImageProcessorDataOut[7] # Object location Y
 
 
-
-
+                        ## make sure not to delay the main loop, sure i could use sleep but that's a loss of cycles.... but you allready knew that.
 			if lastDisplayUpdate+displayUpdateSpeed < time.time(): ## make sure not to delay the main loop, sure i could use sleep but that's a loss of cycles.... but you allready knew that.
-			#sleep(0.1)
-				#rephresh lastDisplayUpdate to current epoch time
-				lastDisplayUpdate=time.time()
+				lastDisplayUpdate=time.time() # Update lastDisplayUpdate with the current epoch time in us
+
+                                # Print statistics.
 				print "lastDisplayUpdate: %s next: %s)" % (lastDisplayUpdate, (time.time()+displayUpdateSpeed))
 				print "BOT Statistics:"
 				print "Ultrasone distance L:%i cm F:%s cm R:%i cm" % ( int(UltrasoneDataOut[0]), int(UltrasoneDataOut[2]), int(UltrasoneDataOut[1]) )
@@ -105,13 +114,15 @@ def main():  #link naar argument parser page, uit ball tracking code
 				print "Object detected: %s"  % ImageProcessorDataOut[4]
 				print "Object Radius: %s" % ImageProcessorDataOut[5]
 				print "Object Location: x:%s y:%s" % (ImageProcessorDataOut[6], ImageProcessorDataOut[7])
-			
 
+			
+## Try to handle and cleanup incase someone hits CTRL+C
 	except KeyboardInterrupt:
 		print "CTRL+C Shutting down..."
 		sensorThread.join()
 		imageProcessorThread.join()
 		perIntelThread.join()
 
+## If I'm started execute main() and let the magick begin.
 if __name__ == '__main__':
 	main()

commit 008e94115d423aa4587714f810512644285a5c83

Fix a bug in Robot.py caused by commit b7e87b632d66ced9e9079cf34a78d419d351bef5

Robot.py
@@ -45,10 +45,10 @@ def main():
         dataManager=Manager() ## Use Manager from multiprocessing to synchronize data objects (basically pass proxies to threads instead of actual arrays, the server is in the parent program)
 
         # Array objects, used for transporting data using proxies to communicate between threads (IPC part)
-	UltrasoneDataOut=dataManager.Array('f', range(7))
+	UltrasoneDataOut=dataManager.Array('f', range(4))
 	ImageProcessorDataOut=dataManager.Array('f', range(9))
-	PerIntelDataIn=dataManager.Array('f', range(4))
-	PerIntelDataOut=dataManager.Array('f', range(4))
+	PerIntelDataIn=dataManager.Array('f', range(7))
+	PerIntelDataOut=dataManager.Array('f', range(7))
 
 ## Initialize and start processes
 	# UltrasoneThread